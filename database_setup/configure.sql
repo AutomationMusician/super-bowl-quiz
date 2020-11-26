5d8c6b912994ea0623525d2007fd939fd8f1d147
joshuafishman <josh.a.fishman@gmail.com>
2020-11-26 14:32:18 -0500

    Add database configuration

---
+++ database_setup/configure.sql
@@ -0,0 +1,16 @@
+CREATE TABLE quizzes (
+  quiz_id INT GENERATED ALWAYS AS IDENTITY,
+  quiz_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY(quiz_id)
+);
+
+CREATE TABLE answers (
+  answer_id INT GENERATED ALWAYS AS IDENTITY,
+  question_id INT NOT NULL,
+  quiz_id INT NOT NULL,
+  response VARCHAR(255) NOT NULL,
+  PRIMARY KEY(answer_id),
+  CONSTRAINT fk_quiz
+    FOREIGN KEY(quiz_id) 
+	  REFERENCES quizzes(quiz_id)
+);
